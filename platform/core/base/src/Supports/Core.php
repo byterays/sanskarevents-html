1fb76e95bb563417f967aad884a6efafb37f66e3
Alien Shrestha <npalien4u@gmail.com>
2025-03-26 19:33:56 +0530

    updated $licenseUrl

--- platform/core/base/src/Supports/Core.php
+++ platform/core/base/src/Supports/Core.php
@@ -75,7 +75,7 @@ final class Core
 
     private string $minimumPhpVersion = '8.2.0';
 
-    private string $licenseUrl = 'https://license.botble.com';
+    private string $licenseUrl = 'https://license.byterays.com';
 
     private string $licenseKey = 'CAF4B17F6D3F656125F9';
 
