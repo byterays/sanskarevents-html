c88afce374f21a6a004eb33c68086d251587c994
Alien Shrestha <npalien4u@gmail.com>
2025-05-24 11:10:42 +0530

    added video gallery

--- video-gallery.php
+++ video-gallery.php
@@ -1,4 +1,8 @@
 <?php include 'header.php'; ?>
+
+<?php 
+$videos = ['Q9lDN-SeNt', 'k3RGyGTlzOs', 'udEH-nEhCdU', 'sKIiZOMV6PA'];
+?>
 <!-- Wrapper Start -->
 <!-- Our Gallery -->
 <section class="ulockd-service-three">
@@ -14,7 +18,7 @@
 		<div class="row">
 			<div class="col-md-12">
 				<!-- Masonry Filter -->
-				<ul class="list-inline masonry-filter text-center">
+				<!-- <ul class="list-inline masonry-filter text-center">
 					<li><a href="#" class="active" data-filter="*">All</a></li>
 					<li><a href="#" data-filter=".events" class="">Events</a></li>
 					<li><a href="#" data-filter=".jimcorbett" class="">Jimcorbett</a></li>
@@ -22,24 +26,26 @@
 					<li><a href="#" data-filter=".himachal" class="">Himachal</a></li>
 					<li><a href="#" data-filter=".bhimtal" class="">Bhimtal</a></li>
 					<li><a href="#" data-filter=".manali" class="">Manali</a></li>
-				</ul>
+				</ul> -->
 				<!-- End Masonry Filter -->
 
 				<!-- Masonry Grid -->
-				<div id="grid" class="masonry-gallery grid-3 mrgn10 clearfix">
+				<div id="grid" class="masonry-gallery video-gallery grid-3 mrgn10 clearfix">
 
 					<!-- Masonry Item -->
+					 <?php foreach ($videos as $video): ?>
 					<div class="isotope-item creative corporate">
-						<div class="ulockd-about-video ulockd-mrgn1225">
+						<div class="ulockd-about-video mrgn10 ulockd-mrgn1225">
 							<div class="ulockd-avdo-thumb">
-								<iframe class="h250" src="http://www.youtube.com/embed/teLhLLlhfzc?autoplay=0"
+								<iframe class="h250" src="http://www.youtube.com/embed/<?php echo $video; ?>?autoplay=0"
 									allowfullscreen=""></iframe>
 							</div>
 						</div>
-						<div class="details text-center">
+						<!-- <div class="details text-center">
 							<h5>Gallery Title Here</h5>							
-						</div>
+						</div> -->
 					</div>
+					<?php endforeach; ?>
 
 				</div>
 				<!-- Masonry Gallery Grid Item -->
